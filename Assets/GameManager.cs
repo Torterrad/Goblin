85a9054c677038935c0ca250b102e10af5dfe157
Torterrad <64802272+Torterrad@users.noreply.github.com>
2023-10-28 01:35:20 +0100

    GameManager attempt
    Score ui and health ui works along with pausing, game over not working yet

---
+++ Assets/GameManager.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//Expat Studios (2020) 2D Retro Platformer Tutorial - Unity - pt.6 (Game Manager & clean up) [Online Video] [Access Date: 4th May 2022] https://youtu.be/LxdH7AZpLLo
+
+public class GameManager : MonoBehaviour
+{
+  
+
+    [Header("Player")]
+    public GameObject player;
+    public Movement movement;
+
+    [Header("Victory/GameOver")]
+
+    public bool hasWon = false;
+    public bool hasLost = false;
+    public GameObject gameOverScreen;
+
+    [Header("Pause")]
+    public GameObject PauseScreen;
+    public bool GameIsPaused = false;
+
+    public GameObject GameUI;
+
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<GameObject>();
+        movement = GameObject.FindGameObjectWithTag("Player").GetComponent<Movement>();
+        player.gameObject.SetActive(true);
+        //GetSceneName();
+        Time.timeScale = 1f;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && (hasWon == false && hasLost == false)) //if the game has neither been won or lost, let the player pause with ESC
+        {
+            if (GameIsPaused) //if paused resume
+            {
+                Resume();
+            }
+            else //otherwise paused
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        //Deactivate pause screen, play music, set Time scale back to normal and now Game is not paused
+        PauseScreen.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        //Same as resume but opposite
+        PauseScreen.SetActive(true);
+
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void GameOver()
+    {
+        //Activate game over screen
+        gameOverScreen.SetActive(true);
+        //Deactivate UI containing all other elements
+        GameUI.GetComponent<Canvas>().enabled = false;
+        //Disable the player, they shouldn't be able to control player after death
+        player.gameObject.SetActive(false);
+
+        GameIsPaused = true; //core functions/updates in the game will halt while this is true
+        hasLost = true; //Player has lost
+
+        Time.timeScale = 0f; //freeze everything
+    }
+
+    public void Restart()
+    {
+        //Restart and deactivate victory or gameover screens
+        gameOverScreen.SetActive(false);
+        //get scene name from the scene and load it
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+    }
+
+    public void LoadMenu()
+    {
+        //reset time scale, game is not longer paused and load scene named Menu
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void LoadInstructions()
+    {
+        SceneManager.LoadScene("Instructions");
+    }
+
+}
---
+++ Assets/Pete's Scripts/Health.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Lives : MonoBehaviour
+{
+    public int lives;
+    public int numOfHearts;
+    public GameManager GameManager;
+
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+
+     void Awake()
+    {
+        GameManager = GameManager.GetComponent<GameManager>();
+    }
+    void Update()
+    {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+
+            if(i < lives)
+            {
+                hearts[i].sprite = fullHeart;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+
+
+            if(i < numOfHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+
+            if(i < 1)
+            {
+                GameManager.GameOver();
+            }
+        }
+    }
+
+
+}
